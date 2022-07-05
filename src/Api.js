7a81036d313fbb61329b0dc8f06bb2adfcdf07c5
izza <swashbuckleizza@gmail.com>
2022-07-05 07:41:35 +0700

    inisialisasi

---
+++ src/Api.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  state = {
+    data: [],
+  };
+
+  //fungsi di dalam kelas dan juga inheritance, dijalankan ketika component mengalami lifecycle tertentu
+  componentDidMount() {}
+
+  //fungsi di dalam kelas dan juga inheritance, dijalankan ketika component mengalami lifecycle tertentu
+  render() {}
+}
